bed063a44e49b266790924f4c53f9ce7f41bdb10
Josh Hancock <livingicon@gmail.com>
2022-08-23 23:02:31 -0400

    Change api request from http to https

--- src/modules/data.js
+++ src/modules/data.js
@@ -22,7 +22,7 @@ const weatherModule = (() => {
 
     togglePosition.checked ? (tempScale = "imperial") : (tempScale = "metric");
     try {
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=${tempScale}&APPID=f59db8f3e672caf010ea9eef7f1433a6`, 
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${tempScale}&APPID=f59db8f3e672caf010ea9eef7f1433a6`, 
       {mode: 'cors'});
       if (!response.ok) throw new Error(`${location} not found.
       Search must be in the form of "City", "City, State" or "City, Country".`);
